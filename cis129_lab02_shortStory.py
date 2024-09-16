23955df477152d69b2101b1610a6c5b4394e8eeb
Landmine15 <156651469+Landmine15@users.noreply.github.com>
2024-09-16 09:03:08 -0700

    added apostrophes to print code cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,8 +1,8 @@
 # Author: Matthew Land
 # My first Python lab in my CIS129 course
 # Prints a simple short story
-print(There once was a dog named Holly)
-print(seeing her made me feel really jolly)
-print(shes such a sweet dog)
-print(who loved to go for walks) 
-print(but her smell tends to be pretty gnarly)      
+print('There once was a dog named Holly')
+print('seeing her made me feel really jolly')
+print('shes such a sweet dog')
+print('who loved to go for walks') 
+print('but her smell tends to be pretty gnarly')      
